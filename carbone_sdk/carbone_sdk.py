49ec40c24d12b26cf6a41ffb38d66551a773c272
Steeve PAYRAUDEAU <spayraudeau@easilys.com>
2020-05-26 15:33:07 +0200

    Added return statements when it throws errors

--- carbone_sdk/carbone_sdk.py
+++ carbone_sdk/carbone_sdk.py
@@ -29,28 +29,35 @@ class CarboneSDK:
   def get_template(self, template_id = None):
     if template_id is None:
       raise ValueError('Carbone SDK get_template error: argument is missing: template_id')
+      return
     with requests.get(self._api_url + "/template/" + template_id,  stream=True, headers=self._api_headers, timeout=self._api_timeout) as r:
       return r.content
 
   def delete_template(self, template_id = None):
     if template_id is None:
       raise ValueError('Carbone SDK delete_template error: argument is missing: template_id')
+      return
     return requests.delete(self._api_url + "/template/" + template_id, headers=self._api_headers, timeout=self._api_timeout)
 
   def render_report(self, template_id = None, json_data = None):
     if template_id is None:
       raise ValueError('Carbone SDK render_report error: argument is missing: template_id')
+      return
     if json_data is None:
       raise ValueError('Carbone SDK render_report error: argument is missing: json_data')
+      return
     return requests.post(self._api_url + "/render/" + template_id, json=json_data, headers=self._api_headers, timeout=self._api_timeout)
 
   def get_report(self, render_id = None):
     if render_id is None:
       raise ValueError('Carbone SDK get_report error: argument is missing: render_id')
+      return
     with requests.get(self._api_url + "/render/" + render_id,  stream=True, headers=self._api_headers, timeout=self._api_timeout) as r:
       return r.content
 
   def generate_template_id(self, template_file_name = None, payload = ""):
+    if template_file_name is None:
+      raise ValueError('Carbone SDK render_report error: argument is missing: template_file_name')
     with open(template_file_name,"rb") as f:
       bytes = f.read() # read entire file as bytes
       h = hashlib.sha256()
@@ -75,7 +82,7 @@ class CarboneSDK:
 
 
 _token = "eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxNjY3IiwiYXVkIjoiY2FyYm9uZSIsImV4cCI6MjIxNzY3NTMwNSwiZGF0YSI6eyJpZEFjY291bnQiOjE2Njd9fQ.ABf57FFgQVX2nwo9gbYIruE17GNtvWKrWsgL7MP_dvgNEYAW5Kr-i9gXVKEBtHNTRtgr_rLHgnkyn4H-JsE2CqI8AXi-T8WGMR5DWdLn352VuA1xge39g9glZpNLVLVGalAZTp-u2ziZTbqlodtfOGzzZSPQnXCmOApsrHWfRhnLyfJX"
-_template_id = "3b912b4f9adffcd8e8abd88dfb4db5c49e89a8cc1e111f6aa661cb3ef1d9459f"
+_template_id = "cb03f7676ef0fbe5d7824a64676166ac2c7c789d9e6da5b7c0c46794911ee7a7"
 
 CSDK = CarboneSDK(_token)
 
@@ -96,11 +103,11 @@ CSDK = CarboneSDK(_token)
 #   print(e)
 
 # DELETE TEMPLATE
-try:
-  resp = CSDK.delete_template(_template_id)
-  print(json.loads(resp.text))
-except Exception as e:
-  print(e)
+# try:
+#   resp = CSDK.delete_template(_template_id)
+#   print(json.loads(resp.text))
+# except Exception as e:
+#   print(e)
 
 # RENDER TEMPLATE
 # try:
